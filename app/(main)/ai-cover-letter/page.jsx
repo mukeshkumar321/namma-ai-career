0ab3e3759a1a5890d6fa084877e2940d839a5858
mukeshkumar321 <mukesh.saroj@sapiens.com>
2025-09-13 20:30:54 +0530

    fix: css

--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -8,7 +8,7 @@ export default async function CoverLetterPage() {
   const coverLetters = await getCoverLetters();
 
   return (
-    <div className="container relative">
+    <div className="container relative px-5">
       <div className="grid-background" />
       <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
         <h1 className="text-6xl font-bold gradient-title">My Cover Letters</h1>
